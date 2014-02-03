8a6ef373bfd297b1429a7135c4c396d7366250f9
Ben Green <ben.green@yale.edu>
2014-02-03 00:44:10 -0500

    galaxy weighting

--- corr_fcn.py
+++ corr_fcn.py
@@ -19,6 +19,7 @@ def getDists(gals,ngal):
 	nboxes = (1000/scale)**3
 	if ngal==0: ngal=gals.shape[0]
 	dists = np.zeros((ngal*ngal)/10.)
+	weights = np.zeros((ngal*ngal)/10.)
 	g = gals[0:ngal]
 	for i in range(nboxes):
 		if i%250==0: print i
@@ -29,16 +30,21 @@ def getDists(gals,ngal):
 				for pi in pointsi:
 					for pj in pointsj:
 						d = dist(pi,pj)
+						w = np.mean( [pi[5], pj[5]] )
 						if d<=150:
-							dists[k] = d
+							dists[k] 	= d
+							weights[k] 	= w
 							k+=1
-	dists = np.trim_zeros(np.sort(np.trim_zeros(dists)))
-	return dists
+	dists = dists[dists!=0] #instead of np.trim_zeros(dists)
+	weights = weights[dists!=0]
+	# weights = weights[dists.argsort()]
+	# dists = np.sort(dists)
+	return dists, weights
 
-# def calcRR(r1,r2):
-# 	univ_vol = 1000**3
-# 	shell_vol = (4./3.)*(np.pi)*(r2**3 - r1**3)
-# 	return (npts**2)*shell_vol/univ_vol
+def calcRR(r1,r2):
+	univ_vol = 1000**3
+	shell_vol = (4./3.)*(np.pi)*(r2**3 - r1**3)
+	return (npts**2)*shell_vol/univ_vol
 
 def calcTPCF(dn,rn):
 	centers = np.zeros((nbins-1))
@@ -47,21 +53,21 @@ def calcTPCF(dn,rn):
 		r1 = bins[i]
 		r2 = bins[i+1]
 		centers[i] = (r1+r2)/2.
-		# RR = calcRR(r1,r2)
+		RR = calcRR(r1,r2) #use for unobserved results?
 		DD = dn[i]
-		RR = rn[i]
+		# RR = rn[i]
 		try:
 			tpcf[i] = np.float(DD)/RR - 1
 		except ZeroDivisionError:
 			tpcf[i] = 0
 	return centers, tpcf
 
-npts = 5000
+npts = 30000
 scale = 100
 
 dfile = '../data/gal_locs.npy'
 dgals = np.load(dfile)
-ddists = getDists(dgals,npts)
+ddists, dweights = getDists(dgals,npts)
 
 rfile = '../data/rand_gals.npy'
 rgals = np.load(rfile)
@@ -69,14 +75,14 @@ rdists = getDists(rgals,npts)
 
 nbins = 75
 bins = np.logspace(np.log10(0.001),np.log10(150),nbins)
-dn, bins = np.histogram(ddists, bins)
+dn, bins = np.histogram(ddists, bins, weights=dweights)
 rn, bins = np.histogram(rdists, bins)
 centers, tpcf1 = calcTPCF(dn,rn)
 # centers, tpcf2 = calcTPCF(n2)
 
-out_file = '../data/dists_pruned_30k.npy'
+out_file = '../data/dists_observed_30k.npy'
 np.save(out_file, dn)
-out_file = '../data/rand_dists_pruned_30k.npy'
+out_file = '../data/rand_dists_observed_30k.npy'
 np.save(out_file, rn)
 
 line1, = py.plot(centers,tpcf1,'bo')
